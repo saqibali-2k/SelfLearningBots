f9ba8e211278d179c1bbb5f315372c81a5ce023e
Saqib Ali <saqibsali00@gmail.com>
2020-08-12 00:37:11 -0400

    changed number of bot battles

--- start_training.py
+++ start_training.py
@@ -6,6 +6,6 @@ from connectfour import Connect4Game, Connect4Model
 if __name__ == '__main__':
     # To prevent unix leakages (prevents Error initialising CUDA)
     mp.set_start_method('spawn')
-    trainer = Trainer(Connect4Model, Connect4Game, num_self_play=100000, num_train_iterations=20,
+    trainer = Trainer(Connect4Model, Connect4Game, num_self_play=100000, num_train_iterations=20, num_bot_battles=100,
                       self_play_cpu=8, bot_battle_cpu=6)
     trainer.training_pipeline()
