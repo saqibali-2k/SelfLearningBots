caa1f806c22da75777f6f420c3837e1c529e049d
Saqib Ali <saqibsali00@gmail.com>
2020-08-12 23:51:11 -0400

    fixed bug where some end conditions were not recognized

--- connectfour.py
+++ connectfour.py
@@ -1,9 +1,7 @@
 from interfaces import *
-from typing import Union
 import numpy as np
 from copy import deepcopy
-import torch
-import tqdm
+from ResidualModel import DefaultModel
 
 EPOCHS = 5
 BATCH_SIZE = 512
@@ -31,6 +29,7 @@ class Connect4Game(Game):
             else:
                 arr = self.p2_array
 
+            i = 0
             for i in range(5, -1, -1):
                 if self.p1_array[i, action] == 0 and self.p2_array[i, action] == 0:
                     arr[i, action] = 1
@@ -54,7 +53,8 @@ class Connect4Game(Game):
             # Switch turns
             self.turn *= -1
 
-    def _follow_line(self, row: int, col: int, dir_x: int, dir_y: int, arr):
+    @staticmethod
+    def _follow_line(row: int, col: int, dir_x: int, dir_y: int, arr):
         count = 0
         while 0 <= row < 6 and 0 <= col < 7 and count != 4:
             if arr[row, col] == 1:
@@ -66,14 +66,21 @@ class Connect4Game(Game):
         return count == 4
 
     def _check_four(self, row: int, col: int, arr):
-        four_in_row = self._follow_line(row, col, -1, 0, arr)
-        four_in_row = self._follow_line(row, col, -1, -1, arr) or four_in_row
-        four_in_row = self._follow_line(row, col, 0, -1, arr) or four_in_row
-        four_in_row = self._follow_line(row, col, 1, -1, arr) or four_in_row
-        four_in_row = self._follow_line(row, col, 1, 0, arr) or four_in_row
-        four_in_row = self._follow_line(row, col, 1, 1, arr) or four_in_row
-        four_in_row = self._follow_line(row, col, 0, 1, arr) or four_in_row
-        four_in_row = self._follow_line(row, col, -1, 1, arr) or four_in_row
+
+        four_in_row = False
+
+        for i in range(3):
+            four_in_row = self._follow_line(row, col + i, -1, 0, arr)
+            four_in_row = self._follow_line(row + i, col + i, -1, -1, arr) or four_in_row
+            four_in_row = self._follow_line(row + i, col, 0, -1, arr) or four_in_row
+            four_in_row = self._follow_line(row + i, col - i, 1, -1, arr) or four_in_row
+            four_in_row = self._follow_line(row, col - i, 1, 0, arr) or four_in_row
+            four_in_row = self._follow_line(row - i, col - i, 1, 1, arr) or four_in_row
+            four_in_row = self._follow_line(row - i, col, 0, 1, arr) or four_in_row
+            four_in_row = self._follow_line(row - i, col + i, -1, 1, arr) or four_in_row
+
+            if four_in_row:
+                return four_in_row
 
         return four_in_row
 
@@ -85,11 +92,12 @@ class Connect4Game(Game):
 
     def get_actions(self):
         lst = []
-        for i in range(7):
-            slots_used = self.p1_array[:, i].sum() + self.p2_array[:, i].sum()
-            assert slots_used <= 6
-            if slots_used != 6:
-                lst.append(i)
+        if not self.is_over():
+            for i in range(7):
+                slots_used = self.p1_array[:, i].sum() + self.p2_array[:, i].sum()
+                assert slots_used <= 6
+                if slots_used != 6:
+                    lst.append(i)
         return lst
 
 
@@ -105,7 +113,7 @@ class Connect4State(State):
     def get_actions(self) -> list:
         return self.game.get_actions()
 
-    def get_representation(self) -> np.ndarray:
+    def get_representation(self) -> tuple:
         return self.game.p1_array, self.game.p2_array
 
     def is_end(self) -> Optional[int]:
@@ -122,175 +130,11 @@ class Connect4State(State):
         return 7
 
 
-class StatWrapper:
+class Connect4WrapperModel(DefaultModel):
 
     def __init__(self):
-        self.total = 0
-        self.count = 0
-
-    def update(self, x):
-        self.total += x
-        self.count += 1
-
-    def __repr__(self):
-        avg = self.total / self.count
-        return f'{avg: .2f}'
-
-
-class ValuePolicyNet(torch.nn.Module):
-    def __init__(self, num_res_blocks: int=3):
-        super(ValuePolicyNet, self).__init__()
-
-        self.conv_layer_1 = torch.nn.Conv2d(3, 256, 3, padding=1)  # input size (3, 6, 7), output: (256, 6, 7)
-        self.batch_norm1 = torch.nn.BatchNorm2d(256)
-        self._num_res_blocks = num_res_blocks
-
-        res_blocks = []
-        for _ in range(num_res_blocks):
-            res_blocks += [torch.nn.Conv2d(256, 256, 3, padding=1)]
-            res_blocks += [torch.nn.BatchNorm2d(256)]
-            res_blocks += [torch.nn.Conv2d(256, 256, 3, padding=1)]
-            res_blocks += [torch.nn.BatchNorm2d(256)]
-            # input: (256, 6, 7), output: (256, 6, 7)
-
-        self.res_blocks = torch.nn.ModuleList(res_blocks)
-        self.relu_activation = torch.nn.functional.relu
-
-        self.fc_layer_policy = torch.nn.Linear(10752, 7)
-
-        self.fc_layer_value1 = torch.nn.Linear(10752, 256)
-        self.fc_layer_value2 = torch.nn.Linear(256, 1)
-
-    def forward(self, states: Union[torch.tensor, np.ndarray]):
-
-        if torch.cuda.is_available():
-            device = torch.device("cuda:0")
-        else:
-            device = torch.device("cpu")
-
-        reshaped_states = torch.tensor(states, dtype=torch.float32, device=device)
-
-        block1 = self.conv_layer_1(reshaped_states)
-        block1 = self.batch_norm1(block1)
-        block1 = self.relu_activation(block1)
-
-        res_in = block1
-        for i in range(self._num_res_blocks):
-            res_out = self.res_blocks[i * 4 + 0](res_in)  # Conv
-            res_out = self.res_blocks[i * 4 + 1](res_out)  # BatchNorm
-            res_out = self.relu_activation(res_out)
-
-            res_out = self.res_blocks[i * 4 + 2](res_out)  # Conv
-            res_out = self.res_blocks[i * 4 + 3](res_out)  # BatchNorm
-            res_out = torch.add(res_out, res_in)
-            res_out = self.relu_activation(res_out)
-            res_in = res_out
-        res_out = res_in
-
-        # Value head
-        value = self.fc_layer_value1(torch.reshape(res_out, (res_out.shape[0], -1)))
-        value = self.relu_activation(value)
-        value = self.fc_layer_value2(value)
-        value = torch.tanh(value)
-
-        # Policy head
-        policy = self.fc_layer_policy(torch.reshape(res_out, (res_out.shape[0], -1)))
-        policy = torch.nn.functional.softmax(policy, dim=1)
-
-        return value, policy
-
-
-class Connect4Model(Nnet):
-    def __init__(self):
-
-        self.model = ValuePolicyNet(3)
-
-        if torch.cuda.is_available():
-            self.model.to(torch.device("cuda:0"))
-        else:
-            self.model.to(torch.device("cpu"))
-
-    def load_weights(self, path: str):
-        self.model.load_state_dict(torch.load(path))
-
-    def save_weights(self, path: str):
-        torch.save(self.model.state_dict(), path)
-
-    def train_model(self, lr, inputs: list, expected_values: list, expected_policies: list):
-        self.model.train()
-        inputs = np.array(inputs)
-        expected_values = np.array(expected_values)
-        expected_policies = np.array(expected_policies)
-
-        if torch.cuda.is_available():
-            device = torch.device("cuda:0")
-        else:
-            device = torch.device("cpu")
-
-        optimizer = torch.optim.Adam(self.model.parameters(), lr=lr, weight_decay=1e-4)
-        steps = 0
-        for i in range(EPOCHS):
-
-            num_batches = expected_values.shape[0] // min(BATCH_SIZE, expected_values.shape[0])
-            p_bar = tqdm.tqdm(range(num_batches), desc=f"Training Network Epoch-{i}")
-
-            rand_indices = torch.randperm(expected_values.shape[0])
-            expected_values = expected_values[rand_indices]
-            expected_policies = expected_policies[rand_indices]
-            inputs = inputs[rand_indices]
-
-            # Stats to report
-            avg_loss = StatWrapper()
-            avg_policy_loss = StatWrapper()
-            avg_value_loss = StatWrapper()
-
-            for _ in p_bar:
-                # Make batch
-                end_index = min((_ + 1) * BATCH_SIZE, expected_values.shape[0])
-
-                value_expected_batch = expected_values[_ * BATCH_SIZE: end_index]
-                policy_expected_batch = expected_policies[_ * BATCH_SIZE: end_index, :]
-                states_batch = inputs[_ * BATCH_SIZE: end_index, :, :]
-
-                policy_expected_batch = torch.tensor(policy_expected_batch, dtype=torch.float32, device=device)
-                value_expected_batch = torch.tensor(value_expected_batch, dtype=torch.float32, device=device)
-
-                # Get outputs
-                value_pred, policy_pred = self.model(states_batch)
-                value_pred = torch.flatten(value_pred)
-
-                # Calculate losses
-                policy_loss = -1 * torch.sum(policy_expected_batch * torch.log(policy_pred + 1e-7))
-                value_loss = (value_pred.to(device) - value_expected_batch).pow(2).sum()
-
-                loss = policy_loss + value_loss
-
-                # Update stats
-                avg_loss.update(loss.item())
-                avg_value_loss.update(value_loss.item())
-                avg_policy_loss.update(policy_loss.item())
-                p_bar.set_postfix(loss=avg_loss, policy_loss=avg_policy_loss, value_loss=avg_value_loss)
-
-                # Perform back prop and update parameters
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
-                steps += 1
-
-        del policy_expected_batch, value_expected_batch
-        del value_pred, policy_pred
-        return steps
-
-    def evaluate(self, inputs):
-        self.model.eval()
-        with torch.no_grad():
-            value, policy = self.model(np.array([inputs]))
-            # Return only the policy vector, and the value scalar
-            return policy.cpu()[0], value.cpu().item()
+        super().__init__(input_size=(3, 6, 7), policy_size=7, num_res_blocks=3)
 
     def action_to_index(self, action):
-        # Actions are numbers from 0 - 6, which corresponds directly to an index
+        # actions range from 0-6 (column on the connect4 board), this is directly mapped to a vector of length 7
         return action
-
-
-
--- start_training.py
+++ start_training.py
@@ -1,11 +1,18 @@
 import torch.multiprocessing as mp
 from Trainer import Trainer
-from connectfour import Connect4Game, Connect4Model
+from connectfour import Connect4Game, Connect4WrapperModel
 
 
 if __name__ == '__main__':
     # To prevent unix leakages (prevents Error initialising CUDA)
     mp.set_start_method('spawn')
-    trainer = Trainer(Connect4Model, Connect4Game, num_self_play=25000, num_train_iterations=200, num_bot_battles=100,
-                      self_play_cpu=8, bot_battle_cpu=6)
+
+    # Self-play until 25000 examples are generated (USE 8 CPU CORES), then train and produce new model. Evaluate new
+    # model in a best of 100 with the old model (USE 6 CPU cores).
+
+    trainer = Trainer(Connect4WrapperModel, Connect4Game, num_self_play=25000, num_train_iterations=200,
+                      num_bot_battles=100, self_play_cpu=8, bot_battle_cpu=6)
+
+    # Bot battle has less processes because it uses more VRAM as each process loads two models instead of one
+
     trainer.training_pipeline()
